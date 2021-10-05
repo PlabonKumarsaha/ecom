01dd4bc27a1f1d5bf98a2bf163e2a21b31f68fe6
PlabonKumarsaha <pkumarsaha71@gmail.com>
2021-10-05 15:51:25 +0600

    added repos

---
+++ src/main/java/com/era/ecom/Repository/OrderDetailsRepository.java
@@ -0,0 +1,9 @@
+package com.era.ecom.Repository;
+
+import com.era.ecom.model.OrderDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderDetailsRepository extends JpaRepository<OrderDetails,Long> {
+}
---
+++ src/main/java/com/era/ecom/service/AdminService.java
@@ -0,0 +1,8 @@
+package com.era.ecom.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminService {
+    
+}
